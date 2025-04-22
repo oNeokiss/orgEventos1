ddfb0347448a3617303919a21c2b95e3e0dcc6a0
gui2112-rgb <guioliveirarosa2112@gmail.com>
2025-04-22 17:16:01 -0300

    Iniciando tela de "lugares"

---
+++ frm_CadLugar.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace orgEventos1
+{
+    partial class frm_CadLugar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(251, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(932, 75);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cadastro de Estabelecimento";
+            // 
+            // frm_CadLugar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1440, 828);
+            this.Controls.Add(this.label1);
+            this.Name = "frm_CadLugar";
+            this.Text = "frm_CadLugar";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
