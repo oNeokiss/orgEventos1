e7b9b4857efd911ed7eeea22a88abed3c6c9bee2
gui2112-rgb <guioliveirarosa2112@gmail.com>
2025-04-25 14:08:42 -0300

    criação da tela de lugares

--- frm_CadLugar.cs
+++ frm_CadLugar.cs
@@ -16,5 +16,10 @@ namespace orgEventos1
         {
             InitializeComponent();
         }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
